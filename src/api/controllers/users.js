f212e5f0d23786b162912f423b93e7aaa96ea09a
Jose Luis Maza <130082810+joseluismaza@users.noreply.github.com>
2024-10-04 22:29:32 +0200

    requisitos añadidos

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -1,73 +1,13 @@
-const { generateSign } = require("../../config/jwt");
-const User = require("../models/users");
-const bcrypt = require("bcrypt");
+const { isAuth, isAdmin } = require("../../middlewares/auth");
+const { register, login, deleteUser, getUser, updateUser } = require("../controllers/users");
 
-const register = async (req, res, next) => {
-  try {
-    const newUser = new User({
-      userName: req.body.userName,
-      password: req.body.password,
-      rol: "user"
-    });
+const usersRoutes = require("express").Router();
 
-    //evitar usuarios duplicados
-    const userDuplicated = await User.findOne({ userName: req.body.userName });
-    if (userDuplicated) {
-      return res.status(400).json("El nombre de usuario ya existe");
-    }
-
-    const userSaved = await newUser.save();
-    return res.status(201).json(userSaved);
-  } catch (error) {
-    return res.status(404).json("Registro incorrecto");
-  }
-}
-
-const login = async (req, res, next) => {
-  try {
-    const userExist = await User.findOne({ userName: req.body.userName });
-
-    if (!userExist) {
-      return res.status(400).json("El usuario o contraseña son incorrectos");
-    }
+usersRoutes.get("/", [isAuth], getUser);
+usersRoutes.post("/register", register);
+usersRoutes.post("/login", login);
+usersRoutes.delete("/:id", [isAdmin], deleteUser);
+usersRoutes.put("/:id", updateUser);
 
 
-    if (userExist) {
-      if (bcrypt.compareSync(req.body.password, userExist.password)) {//comparación de contraseñas
-        //login jsonwebtoken
-        const token = generateSign(userExist._id);
-        return res.status(200).json({ userExist, token });
-      } else {
-        return res.status(400).json("El usuario o contraseña son incorrectos");
-      }
-    }
-  } catch (error) {
-    return res.status(404).json(error);
-
-  }
-
-}
-
-const deleteUser = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const userDeleted = await User.findByIdAndDelete(id);
-    return res.status(200).json({
-      mensaje: "El usuario ha sido eliminado",
-      userDeleted,
-    })
-  } catch (error) {
-    return res.status(400).json(error);
-  }
-}
-
-const getUser = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    return res.status(200).json(users);
-  } catch (error) {
-    return res.status(400).json(error);
-  }
-}
-
-module.exports = { register, login, deleteUser, getUser }
+module.exports = usersRoutes;
