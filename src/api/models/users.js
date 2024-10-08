d51688b4225dd70bfe27fca17281a9fbf9ee548b
Jose Luis Maza <130082810+joseluismaza@users.noreply.github.com>
2024-10-08 18:52:25 +0200

    archivo correcto

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -1,32 +1,13 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const { isAuth, isAdmin } = require("../../middlewares/auth");
+const { register, login, deleteUser, getUser, updateUser } = require("../controllers/users");
 
-const userSchema = new mongoose.Schema(
-  {
-    userName: { type: String, required: true },
-    password: { type: String, required: true },
-    rol: {
-      type: String,
-      required: true,
-      enum: ["admin", "user"],
-      default: "user",
-    },
-  },
-  //relación entre colecciones
-  {
-    libreria: { type: mongoose.Schema.Types.ObjectId, ref: "Libreria" }, //vincular usuario con libreria en concreto
-    comicsComprados: [{ type: mongoose.Schema.Types.ObjectId, ref: "comics" }] //array de comics comprados por el usuario
+const usersRoutes = require("express").Router();
 
-  },
-  {
-    timestamps: true,
-    collection: "users",
-  }
-);
+usersRoutes.get("/", [isAuth], getUser);
+usersRoutes.post("/register", register);
+usersRoutes.post("/login", login);
+usersRoutes.delete("/:id", [isAdmin], deleteUser);
+usersRoutes.put("/:id", updateUser);
 
-userSchema.pre("save", function () {
-  this.password = bcrypt.hashSync(this.password, 10);//número de saltos que se encripta la contraseña
-})
 
-const User = mongoose.model("users", userSchema, "users");
-module.exports = User;
+module.exports = usersRoutes;
